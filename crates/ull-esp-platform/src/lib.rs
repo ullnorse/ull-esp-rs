0ee0069d3678a425719f7d0e6f5cc81864cc5d66
Aleksa Jonic <ullnorse@gmail.com>
2026-06-17 08:15:33 +0200

    feat: add platform heap definition macro

--- crates/ull-esp-platform/src/lib.rs
+++ crates/ull-esp-platform/src/lib.rs
@@ -1,6 +1,24 @@
 #![no_std]
 
 extern crate alloc;
+pub extern crate esp_alloc as __esp_alloc;
+
+#[macro_export]
+macro_rules! define_heap {
+    ($vis:vis $name:ident, $size:expr) => {
+        $vis fn $name() {
+            static mut HEAP: core::mem::MaybeUninit<[u8; $size]> = core::mem::MaybeUninit::uninit();
+
+            unsafe {
+                $crate::__esp_alloc::HEAP.add_region($crate::__esp_alloc::HeapRegion::new(
+                    HEAP.as_mut_ptr() as *mut u8,
+                    $size,
+                    $crate::__esp_alloc::MemoryCapability::Internal.into(),
+                ));
+            }
+        }
+    };
+}
 
 pub use config::WifiConfig;
 pub use error::EspError;
